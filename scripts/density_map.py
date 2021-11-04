9260dba727e8445122287dfbc6879b262d8863fa
SebastianJL <johannes.lade@yahoo.com>
2021-11-04 21:17:45 +0100

    Updated paths in density_map.py.

--- scripts/density_map.py
+++ scripts/density_map.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-mass_grid_modulo = np.genfromtxt('mass_grid_modulo_2d.csv', delimiter=',')
-mass_grid_if_else = np.genfromtxt('mass_grid_if_else_2d.csv', delimiter=',')
-mass_grid_margins = np.genfromtxt('mass_grid_with_margins_2d.csv', delimiter=',')
+mass_grid_modulo = np.genfromtxt('../output/mass_grid_modulo_2d.csv', delimiter=',')
+mass_grid_if_else = np.genfromtxt('../output/mass_grid_if_else_2d.csv', delimiter=',')
+mass_grid_margins = np.genfromtxt('../output/mass_grid_with_margins_2d.csv', delimiter=',')
 
 datas = [np.log(mass_grid_modulo + 1),
          np.log(mass_grid_if_else + 1),
@@ -12,7 +12,7 @@ datas = [np.log(mass_grid_modulo + 1),
 for data in datas:
     print(f'{data.shape = }')
 
-fig, axes = plt.subplots(1, 3, figsize=(15, 15))
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 titles = ['modulo', 'if else', 'with margins']
 
 for ax, title, data in zip(axes, titles, datas):
@@ -33,10 +33,12 @@ diffs = [
 print(f'{np.max(np.abs((diff_modulo_if_else))) = }')
 print(f'{np.max(np.abs((diff_modulo_margins))) = }')
 
-fig, axes = plt.subplots(1, 2, figsize=(15, 15))
+fig, axes = plt.subplots(1, 2, figsize=(15, 5))
 titles = ['modulo - if-else', 'modulo - margins']
 
 for ax, title, data in zip(axes, titles, diffs):
     pos = ax.imshow(data, cmap='PiYG')
     ax.title.set_text(title)
     fig.colorbar(pos, ax=ax, shrink=0.5)
+
+plt.show()

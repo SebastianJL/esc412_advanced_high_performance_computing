4e7c94f128fd138b3d2e15fff2b9ed31ad41fcab
SebastianJL <johannes.lade@yahoo.com>
2021-11-24 12:52:23 +0100

    Remove local blitz build and replace with system-wide library.

--- blitz-1.0.2/blitz/indexmap-forward.h
+++
@@ -1,9 +0,0 @@
-#ifndef BZ_INDEXMAP_FWD
-#define BZ_INDEXMAP_FWD
-
-namespace blitz {
-
-
-}
-
-#endif
--- blitz-build/blitz/array/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johannes/uni/msc/uzh/HS21/ahpc/project/blitz-1.0.2/blitz/array
-# Build directory: /home/johannes/uni/msc/uzh/HS21/ahpc/project/blitz-build/blitz/array
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

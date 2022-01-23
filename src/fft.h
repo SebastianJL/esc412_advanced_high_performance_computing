8e95518d025408a5f53dc632b1adf9495cdeb1bd
SebastianJL <johannes.lade@yahoo.com>
2022-01-23 14:34:41 +0100

    Sync function signatures in fft.cu and fft.h.

--- src/fft.h
+++ src/fft.h
@@ -12,8 +12,8 @@ typedef std::complex<real_type> complex_type;
 typedef blitz::Array<real_type,3> array3D_r;
 typedef blitz::Array<complex_type,3> array3D_c;
 
-void compute_fft_2D_R2C(array3D_r &grid, int N, int Nx);
-void compute_fft_1D_C2C(array3D_c &fft_grid, int N, int Nx);
+void compute_fft_2D_R2C(array3D_r &grid, int N, int local_n);
+void compute_fft_1D_C2C(array3D_c &fft_grid, int N, int local_n);
 void compute_fft_2D_R2C_stream(array3D_r &grid, array3D_c &fft_grid, int N);
 
 
